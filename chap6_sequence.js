f186070b6342b3539524a29d077e6f54a43ca282
Davi L. Matos <64372533+davilmatos@users.noreply.github.com>
2020-05-20 22:17:00 -0300

    Create chap6_sequence.js

---
+++ chap6_sequence.js
@@ -0,0 +1,32 @@
+//I don't know if I did the code the way it was expected in the exercise, but I got the expected results...
+//creating the function logFive
+function logFive(arr) {
+    for(let i = 0; i < Math.min(5, arr.length); i++) {
+        console.log(arr[i]);
+    };
+};
+
+//creating class ArraySeq
+class ArraySeq {
+    constructor(arr) {
+        return arr;
+    }
+};
+
+//creating class RangeSeq
+class RangeSeq {
+    constructor(a, b) {
+        return Array.from(Array(b + 1).keys()).slice(a)
+        }
+};
+
+//tests
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
